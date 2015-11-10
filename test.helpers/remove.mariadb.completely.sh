2a20af7493c59122e0615f0defd49928b3001b1f
moosecomputing <keith@ncoo.org>
2015-11-10 15:55:29 -0800

    Minor change to remove mariadb script.

--- test.helpers/remove.mariadb.completely.sh
+++ test.helpers/remove.mariadb.completely.sh
@@ -8,4 +8,6 @@ sudo /etc/init.d/mysql stop
 sudo apt-get -y remove --purge 'mariadb*'
 sudo apt-get -y clean
 sudo apt-get -y autoremove
+
+#this is important. If you don't get rid of the old dbs, they will not get overwritten.
 sudo rm -r /var/lib/mysql/*
